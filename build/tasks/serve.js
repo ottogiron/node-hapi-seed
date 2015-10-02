eaa9589ba2dae0adeb29a6e120934e15eb96d92a
Otto Giron <ottog2486@gmail.com>
2015-10-01 20:17:35 -0600

    Remove unnecesary files

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,10 +1,7 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
-// this task utilizes the browsersync plugin
-// to create a dev server instance
-// at http://localhost:9000
-gulp.task('serve', ['build', 'watch'], function() {
+gulp.task('serve', ['watch'], function() {
   nodemon({ script: 'server',
             ext: 'html js',
 })
