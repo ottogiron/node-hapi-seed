56f845abc31bc6a9cb15b807d1e30fd8a9a26178
Otto Giron <otto.giron@xoom.com>
2015-10-24 13:09:45 -0600

    Change dotenv entry point

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,4 +1,3 @@
-require('dotenv').load();
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 // all gulp tasks are located in the ./build/tasks directory
 // gulp configuration is in files in ./build directory
+require('dotenv').load();
 require('require-dir')('build/tasks');
